52c0fb49548786a30f96ea7fe75600eb2b1f0d8e
Bartosz Wos <bartosz.wos.cs@gmail.com>
2026-03-01 05:45:12 +0100

    chore: add remaining branch files

---
+++ ml/live_data_sniper.py
@@ -0,0 +1,43 @@
+import asyncio
+import websockets
+import json
+import csv
+from datetime import datetime
+
+async def data_sniper():
+    url = "wss://stream.binance.com:9443/ws/btcusdt@depth5@100ms"
+    filename = "ml/binance_top5_data.csv"
+
+    print("--- Starting Binance WS Monitoring  ---")
+
+    with open(filename, mode='a', newline='') as f:
+        writer = csv.writer(f)
+
+        async with websockets.connect(url) as ws:
+            count = 0
+            while True:
+                try:
+                    response = await ws.recv()
+                    data = json.loads(response)
+
+                    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+
+                    bids = [it for lst in data['bids'] for it in lst]
+                    asks = [it for lst in data['asks'] for it in lst]
+
+                    row = [time] + bids + asks
+                    writer.writerow(row)
+
+                    count += 1
+
+                    if count % 100 == 0:
+                        print(f"[{time}] Got {count} samples...")
+
+                except Exception as e:
+                    print(f"Connection error: {e}")
+                    break
+if __name__ == "__main__":
+    try:
+        asyncio.run(data_sniper())
+    except KeyboardInterrupt:
+        print("\n--- Monitoring finished ---")
